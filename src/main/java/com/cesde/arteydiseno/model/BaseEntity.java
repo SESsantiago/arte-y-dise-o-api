6ab279febd86145d89c4326e90ed5d2b03369b89
SESsantiago <santyespinosa03@gmail.com>
2026-08-19 23:50:21 -0500

    Se crea la clase obligatoria con @MappedSuperclass que tendra los atributos transversales

--- src/main/java/com/cesde/arteydiseno/model/BaseEntity.java
+++ src/main/java/com/cesde/arteydiseno/model/BaseEntity.java
@@ -1,4 +1,39 @@
-package com.cesde.arteydiseno.model.base;
+package com.cesde.arteydiseno.model;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaActualizacion;
+
+    @Column(nullable = false)
+    private Boolean estadoActivo = true;
+
+    @PrePersist
+    protected void onCreate() {
+        fechaCreacion = LocalDateTime.now();
+        fechaActualizacion = LocalDateTime.now();
+        if (estadoActivo == null) estadoActivo = true;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        fechaActualizacion = LocalDateTime.now();
+    }
 }
