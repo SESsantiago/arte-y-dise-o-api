aa12987652fe3d8613e93d2bc466c2496b2887f3
Andrés Giraldo <agiraldofe1@cesde.net>
2026-08-23 19:10:47 -0500

    revert: Distribuir pagos y usuario

--- src/main/java/com/cesde/arteydiseno/model/entity/Pagos.java
+++ src/main/java/com/cesde/arteydiseno/model/entity/Pagos.java
@@ -1,33 +1,4 @@
 package com.cesde.arteydiseno.model.entity;
 
-import com.cesde.arteydiseno.model.base.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-public class Pagos extends BaseEntity {
-
-    @Column(nullable = false, precision = 12, scale = 2)
-    private BigDecimal monto;
-
-    @Column(nullable = false)
-    private LocalDateTime fechaPago;
-
-    @Column(length = 100)
-    private String referencia;
-
-    @ManyToOne
-    @JoinColumn(name = "factura_id", nullable = false)
-    private Factura factura;
-
-    @ManyToOne
-    @JoinColumn(name = "metodo_pago_id", nullable = false)
-    private MetodosPago metodoPago;
+public class Pagos {
 }
--- src/main/java/com/cesde/arteydiseno/model/entity/Usuario.java
+++ src/main/java/com/cesde/arteydiseno/model/entity/Usuario.java
@@ -1,27 +1,4 @@
 package com.cesde.arteydiseno.model.entity;
 
-import com.cesde.arteydiseno.model.base.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Entity
-public class Usuario extends BaseEntity {
-
-    @Column(nullable = false, unique = true, length = 120)
-    private String email;
-
-    @Column(nullable = false)
-    private String passwordHash;
-
-    @Column(nullable = false, length = 30)
-    private String rol;
-
-    @OneToOne
-    @JoinColumn(name = "cliente_id", unique = true)
-    private Cliente cliente;
+public class Usuario {
 }
