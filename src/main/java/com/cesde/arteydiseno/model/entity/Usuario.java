8e9f4768a428df44572eb1fa9d1cbd8812dfc66c
diegoh-11 <dherreraal12@cesde.net>
2026-09-01 15:24:28 -0500

    Entidad Usuario

--- src/main/java/com/cesde/arteydiseno/model/entity/Usuario.java
+++ src/main/java/com/cesde/arteydiseno/model/entity/Usuario.java
@@ -1,4 +1,27 @@
 package com.cesde.arteydiseno.model.entity;
 
-public class Usuario {
+import com.cesde.arteydiseno.model.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Usuario extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 30)
+    private String rol;
+
+    @OneToOne
+    @JoinColumn(name = "cliente_id", unique = true)
+    private Cliente cliente;
 }
